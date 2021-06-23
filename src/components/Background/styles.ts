f5b9457c1e1bbba4bc2b6470f38ce4f0713613b8
Joao Vitor Pinto Andrade <joaodbv@outlook.com>
2021-06-23 17:15:27 -0300

    (Correções gerais)

---
+++ src/components/Background/styles.ts
@@ -0,0 +1,8 @@
+import { Dimensions, StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    height: Dimensions.get("window").height,
+  },
+});
